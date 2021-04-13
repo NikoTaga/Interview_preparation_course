00b55f2609b31cbb9a721a9900a0df304911a6ae
NikoTaga <kraffchenko@mail.ru>
2021-04-13 19:49:20 +0300

    'task_6'

--- lesson_2.py
+++ lesson_2.py
@@ -29,9 +29,32 @@ class ItemDiscountReport(ItemDiscount):
     def __str__(self):
         return f'Товар - {self.get_name()}, цена - {self.calc_disc_price()}'
 
+    def get_parent_data(self):
+        return self.__str__()
+
+    def get_info(self):
+        return f'Наименование - {self.get_name()}'
+
+
+class ItemDiscountReport1(ItemDiscount):
+
+    def __init__(self, name, price, discount):
+        super().__init__(name, price)
+        self.__name = name
+        self.__price = price
+        self.__discount = discount
+
+    def calc_disc_price(self):
+        return self.get_price() - self.get_price() * 0.01 * self.__discount
+
+    def __str__(self):
+        return f'Товар - {self.get_name()}, цена - {self.calc_disc_price()}'
 
     def get_parent_data(self):
         return self.__str__()
+
+    def get_info(self):
+        return f'Цена - {self.get_price()}'
 
 
 
@@ -49,3 +72,13 @@ if __name__ == '__main__':
     product = ItemDiscountReport(a, b, d)
     print(product.get_name(), product.get_price())
     print(product.get_parent_data())
+    print('--------')
+
+    print(product.get_info())
+
+    product1 = ItemDiscountReport(a, b, d)
+    print(product1.get_info())
+
+    product_list = [product, product1]
+    for i in product_list:
+        print(i.get_info())
