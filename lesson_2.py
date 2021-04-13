8893b4506c339ae4b66cc126a93c30ae80dc618b
NikoTaga <kraffchenko@mail.ru>
2021-04-13 18:38:01 +0300

    'task_1'

--- lesson_2.py
+++ lesson_2.py
@@ -1,1 +1,20 @@
 
+class ItemDiscount:
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class ItemDiscountReport(ItemDiscount):
+
+    def get_parent_data(self):
+        return f'Товар - {self.name}, цена - {self.price}'
+
+
+
+if __name__ == '__main__':
+    a = 'Лошадь'
+    b = 2000
+    product = ItemDiscountReport(a, b)
+    print(product.get_parent_data())
