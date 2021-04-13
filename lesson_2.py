26201a16120f51010258c7658d8910d25cb01971
NikoTaga <kraffchenko@mail.ru>
2021-04-13 18:45:19 +0300

    'task_2'

--- lesson_2.py
+++ lesson_2.py
@@ -2,19 +2,22 @@
 class ItemDiscount:
 
     def __init__(self, name, price):
-        self.name = name
-        self.price = price
+        self.__name = name
+        self.__price = price
 
 
 class ItemDiscountReport(ItemDiscount):
 
     def get_parent_data(self):
-        return f'Товар - {self.name}, цена - {self.price}'
+        return f'Товар - {self.__name}, цена - {self.__price}'
 
 
 
 if __name__ == '__main__':
     a = 'Лошадь'
     b = 2000
+    product_par = ItemDiscount(a, b)
+    print(product_par.name, product_par.price)
+
     product = ItemDiscountReport(a, b)
     print(product.get_parent_data())
